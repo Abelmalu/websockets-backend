2a475bcc800d27c36df809cc40e975c0f6452ece
abel <abellmalu@gmail.com>
2025-03-24 14:55:53 +0300

    login and logout logic updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,12 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
-    public function Register(Request $request){
 
-       $validator =  Validator::make($request->all(),[
-            "username" => 'unique:users|required|max:255|string|required',
+    public function Register(Request $request)
+    {
+
+        $validator =  Validator::make($request->all(), [
+            "username" => 'required|max:255|unique:users,username|string',
             "name" => 'required|max:255|string|required',
             'identifier' => 'required|unique:users,email|unique:users,phone',
             "password" => 'required|confirmed',
@@ -22,8 +23,11 @@ class AuthController extends Controller
 
         ]);
         // Determine if the identifier is an email or phone number
+
+
         $data = [
             'name' => $request->name,
+            'username' => $request->username,
             'password' => Hash::make($request->password),
         ];
 
@@ -42,7 +46,9 @@ class AuthController extends Controller
 
             $user = new User($data);
 
+
             $user->save();
+
 
 
             $token = $user->createToken($request->name);
@@ -56,8 +62,58 @@ class AuthController extends Controller
 
             ];
         }
+    }
+
+
+    public function login(Request $request)
+    {
+
+        Validator::make($request->all(), [
+
+            'identifier' => 'required',
+            'password' => 'required'
+
+        ]);
 
 
 
+        $identifier = $request->identifier;
+        $password = $request->password;
+
+        // Find user by email, phone, or username
+        $user = User::where('email', $identifier)
+            ->orWhere('phone', $identifier)
+            ->orWhere('username', $identifier)
+            ->first();
+
+
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+
+            return ['message' => 'The provided credentials are incorrect'];
+        }
+
+        $token = $user->createToken($user->name);
+
+
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+
+
+        ];
+    }
+
+
+    public function logout(Request $request)
+    {
+
+
+
+
+        $request->user()->tokens()->delete();
+
+        return ['message' => 'You are logged Out'];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
@@ -7,4 +8,6 @@ use App\Http\Controllers\MessageController;
 
 
 Route::get('/chatmessages',[MessageController::class,'messages']);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 Route::post('/message',[MessageController::class,'createMessage']);
