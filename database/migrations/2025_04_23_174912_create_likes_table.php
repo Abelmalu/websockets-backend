34a2840e06c9dd701b73d1599e5d81efc23fc68a
abel <abellmalu@gmail.com>
2025-04-28 23:50:35 +0300

    likes relationship created

--- database/migrations/2025_04_23_174912_create_likes_table.php
+++ database/migrations/2025_04_23_174912_create_likes_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
+            $table->foreignId('post_id')->constrained();
             $table->id();
             $table->timestamps();
         });
